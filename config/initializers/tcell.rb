6e9eb9ad6490b3dcaa98ffc9110f9e5392a4e522
Rafael Alba <rafael@tcell.io>
2017-01-24 15:33:16 -0800

    easier to keep logging options in tcell initializer

--- config/initializers/tcell.rb
+++ config/initializers/tcell.rb
@@ -1,6 +1,7 @@
 if defined?(TCellAgent)
   TCellAgent.configure do |config|
-    config.logger = Logger.new(STDOUT)
-    config.logger.level = 5
+    config.logging_options = {"enabled" => true, "level" => "DEBUG"}
+    #config.logger = Logger.new(STDOUT)
+    #config.logger.level = 0
   end
 end
