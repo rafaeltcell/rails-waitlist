73388499164bf06f6e6d934732c0834b2073ea28
Rafael Alba <rafael@tcell.io>
2017-05-05 12:57:43 -0700

    disable event manager

--- config/initializers/tcell.rb
+++ config/initializers/tcell.rb
@@ -1,5 +1,6 @@
 if defined?(TCellAgent)
   TCellAgent.configure do |config|
     config.logging_options = {"enabled" => true, "level" => "DEBUG"}
+    config.enable_event_manager = false
   end
 end
